baae3352687c0a6186bc86d444cd4913823ad9fc
theajack <1506282385@qq.com>
2021-09-09 02:31:26 +0800

    feat: 删除无效代码 修改polygon.lines 属性名

--- src/geometry/graph/polygon.ts
+++ src/geometry/graph/polygon.ts
@@ -2,7 +2,7 @@
  * @Author: theajack
  * @Date: 2021-08-12 23:21:02
  * @LastEditor: theajack
- * @LastEditTime: 2021-09-09 02:21:46
+ * @LastEditTime: 2021-09-09 02:30:32
  * @Description: Coding something
  * @FilePath: \tc-image\src\geometry\graph\polygon.ts
  */
@@ -15,20 +15,20 @@ import {isOddNumber} from '../../utils/math';
 export class Polygon extends Graph implements IPolygon {
     
     points: IPoint[];
-    lines: Line[];
+    edges: Line[];
     boundary: Rect;
 
     constructor (points: IPoint[]) {
         super();
         if (points.length < 3) {throw new Error('Wrong Polygon');};
         this.points = points;
-        this.lines = [];
+        this.edges = [];
         this.initBoundary(points[0]);
         points.forEach((point, index) => {
             this.expendBoundary(point);
             const nextIndex = (index < points.length - 1) ? (index + 1) : 0;
             const nextPoint = points[nextIndex];
-            this.lines.push(new Line({
+            this.edges.push(new Line({
                 start: point,
                 end: nextPoint
             }));
@@ -57,8 +57,8 @@ export class Polygon extends Graph implements IPolygon {
         if (!this.boundary.isContainPoint(point)) return false;
 
         // 如果点在多边形的边上，则返回true
-        for (let i = 0; i < this.lines.length; i++) {
-            if (this.lines[i].isContainPoint(point))
+        for (let i = 0; i < this.edges.length; i++) {
+            if (this.edges[i].isContainPoint(point))
                 return true;
         }
         // 射线法判断点是否在多边形内
@@ -70,7 +70,7 @@ export class Polygon extends Graph implements IPolygon {
 
     countIntersectPointNumberWithLine (anotherLine: Line) {
         let sum = 0;
-        this.lines.forEach((line: Line, index: number) => {
+        this.edges.forEach((line: Line, index: number) => {
             if (this.isAnotherLineCrossEdge(anotherLine, line, index)) {
                 sum ++;
             }
@@ -80,15 +80,15 @@ export class Polygon extends Graph implements IPolygon {
 
     private prevLine (index: number|Line): Line {
         if (typeof index !== 'number') {
-            index = this.lines.indexOf(index);
+            index = this.edges.indexOf(index);
         }
-        return this.lines[(index === 0 ? this.lines.length : index) - 1];
+        return this.edges[(index === 0 ? this.edges.length : index) - 1];
     }
 
-    countIntersectPointNumberWithLines (lines: Line[]) {
+    countIntersectPointNumberWithLines (edges: Line[]) {
         const sums: number[] = [];
-        this.lines.forEach((line, lineIndex) => {
-            lines.forEach((anotherLine, index) => {
+        this.edges.forEach((line, lineIndex) => {
+            edges.forEach((anotherLine, index) => {
                 if (typeof sums[index] !== 'number') sums[index] = 0;
                 if (this.isAnotherLineCrossEdge(anotherLine, line, lineIndex)) {
                     sums[index] ++;
@@ -98,21 +98,15 @@ export class Polygon extends Graph implements IPolygon {
         return sums;
     }
 
-    isAnotherLineCrossEdge (anotherLine: Line, edge: Line, edgeIndex?: number) {
-        if (typeof edgeIndex !== 'number') edgeIndex = this.lines.indexOf(edge);
+    private isAnotherLineCrossEdge (anotherLine: Line, edge: Line, edgeIndex?: number) {
+        if (typeof edgeIndex !== 'number') edgeIndex = this.edges.indexOf(edge);
         
         const intersectResult = anotherLine.isIntersectAnotherLine(edge, {includeStartPoint: true});
 
         if (intersectResult.isTrue) {
-            // 不与边的起点相交 或者 当前边终点与上一条非重合边的起点在线段两侧
-            if (
-                !intersectResult.isStartOnLine ||
-                anotherLine.isSplit2Point(this.prevLine(edgeIndex).start, edge.end).isTrue
-            ) {
-                return true;
-            }
-
             if (intersectResult.isStartOnLine) {
+                // 如果边的起点相交
+                // 则还需与当前边终点与上一条非重合边的起点在线段两侧
                 let prevLine = this.prevLine(edgeIndex);
                 while (prevLine.isCoincideAnotherLine(anotherLine)) {
                     prevLine = this.prevLine(prevLine);
@@ -123,23 +117,7 @@ export class Polygon extends Graph implements IPolygon {
 
                 return false;
             }
-
             return true;
-
-            // if(intersectResult.isStartOnLine){
-            //     return false;
-            // }
-
-            // if (!intersectResult.isStartOnLine) {
-            //     let prevLine = this.prevLine(edgeIndex);
-            //     while (prevLine.isCoincideAnotherLine(anotherLine)) {
-            //         prevLine = this.prevLine(edgeIndex);
-            //     }
-
-            //     if (anotherLine.isSplit2Point(prevLine.start, edge.end).isTrue) {
-            //         return true;
-            //     }
-            // }
         }
         return false;
     }
@@ -164,18 +142,3 @@ export class Polygon extends Graph implements IPolygon {
         };
     }
 }
-
-// const p = new Polygon([
-//     {x: 0, y: 400},
-//     {x: 400, y: 200},
-//     {x: 200, y: 400},
-//     {x: 400, y: 600}
-// ]);
-
-// p.isAnotherLineCrossEdge(
-//     {
-//         start: {x: 400, y: 400},
-//         end: {x: 400, y: 0}
-//     },
-//     p.lines[1]
-// );
